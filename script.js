1db439ef84703483fd94b10ecaaf7955b0feaab0
Feyisara Agiri <mohrenny8@gmail.com>
2025-06-09 15:24:24 +0100

    i made some corrections

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-document.addEventListener("DomContextLoaded", function () {
+document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("registration-form");
   const feedbackDiv = document.getElementById("form-feedback");
 
